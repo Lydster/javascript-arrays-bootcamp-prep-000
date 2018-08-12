c6fb9c47b69ed0fefd91c3654f4dcbcc9a4c6696
Lydia Thornton <lydia.d.thornton@gmail.com>
2018-08-12 05:40:46 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,19 @@
+function addElementToBeginningOfArray(array, element) {
+  return ['foo', 1]
+
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return ['foo', 1]
+}
+
+var chocolateBar = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToEndOfArray(array, element) {
+  return [1, 'foo']
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return [1 , 'foo']
+}
+
