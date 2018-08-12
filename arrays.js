01789d15e894823681b4bde6e53afc70d112b3ef
Lydia Thornton <lydia.d.thornton@gmail.com>
2018-08-12 06:25:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray(array, element) {
+function addElementToBeginningOfArray(element, array) {
   chocolateBars.unshift("Dove Bar")
 }
 
-
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  chocolateBars.unshift("Candy")
+}
