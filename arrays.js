279eef14f88808e318df1e68731e4dd61db92ae6
Lydia Thornton <lydia.d.thornton@gmail.com>
2018-08-12 06:13:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-  return chocolateBars.push("Dove Bar")
+  chocolateBars.push("Dove Bar")
 }
 
 
