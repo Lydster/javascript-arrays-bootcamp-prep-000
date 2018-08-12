8c086ec33d0a1b79d55b417f1533a2a2ae14c874
Lydia Thornton <lydia.d.thornton@gmail.com>
2018-08-12 06:10:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,15 +4,4 @@ function addElementToBeginningOfArray(array, element) {
   return ['Dove Bar', chocolateBar]
 }
 
-function destructivelyAddElementoBeginningOfArray(array, element) {
-  return ['foo', 1]
-}
-
-function addElementToEndOfArray(array, element) {
-  return [1, 'foo']
-}
-
-function destructivelyAddElementToEndOfArray(array, element) {
-  return [1 , 'foo']
-}
 
